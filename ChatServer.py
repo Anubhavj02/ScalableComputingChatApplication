2451d2044e27e1a440585ac9fd6dadd1de45a98e
Anubhav Jain <jainan@tcd.ie>
2017-11-04 18:51:34 +0000

    Commit5: User chat room leave and response message

--- ChatServer.py
+++ ChatServer.py
@@ -5,10 +5,11 @@ from threading import Lock  # For implementing Thread lock
 import re as regex  # For pattern matching of input strings from the client
 import ChatRoom  # Python class having all functionalities of a ChatRoom
 import hashlib  # For unique numbers associated to a string
+import os  # for using the exit function
 
 # Global variables for port and host
 port = 0
-host = "localhost"
+host = "134.226.44.159"
 
 # All client stored in the queue
 clients = Queue()
@@ -16,15 +17,18 @@ clients = Queue()
 # This array will contain all the details of chat rooms in the memory
 chat_rooms_array = {}
 
-# This thread lock will hold other threads until a thread has finished processing, it would create synchronizing Chat room creation and chats
+# This thread lock will hold other threads until a thread has finished processing,
+# it would create synchronizing Chat room creation and chats
 rooms_lock = Lock()
 
 # All the valid input messages to the server from the client
-valid_join_msg = r"JOIN_CHATROOM: ?(.*)\\nCLIENT_IP: ?(.*)\\nPORT: ?(.*)\\nCLIENT_NAME: ?(.*)\\n"
-valid_hello_msg = r"HELO ?(.*)\\n"
+valid_join_msg = r"JOIN_CHATROOM: ?(.*)(?:\s|\\n)CLIENT_IP: ?(.*)(?:\s|\\n)PORT: ?(.*)(?:\s|\\n)CLIENT_NAME: ?(.*)"
+valid_hello_msg = r"HELO ?(.*)\s"
+valid_leave_msg = r"LEAVE_CHATROOM: ?(.*)(?:\s|\\n)JOIN_ID: ?(.*)(?:\s|\\n)CLIENT_NAME: ?(.*)"
 
 # Various error codes and their description
-error_code_5 = "Invalid Message Format... Please write the correct format"
+error_code_1 = "Invalid Message Format... Please write the correct format"
+error_code_2 = "No Chat room exists with the Chat Room Ref-"
 
 
 # Client thread to handle multiple client requests
@@ -48,7 +52,6 @@ class ClientThread(Thread):
 
     @staticmethod
     def process_message(conn, addr):
-
         """function to process a client request
 
         Args:
@@ -68,11 +71,19 @@ class ClientThread(Thread):
             # Check if message string have some data or not
             if len(message) > 0:
                 print "MESSAGE FROM CLIENT->", message
+
+                #  Check the type of message and process them accordingly
+                if message == "KILL_SERVICE\n":
+                    os._exit(0)
+
                 if message.startswith("HELO"):
                     process_hello_msg(conn, message, addr)
 
                 if message.startswith("JOIN_CHATROOM"):
                     process_join_msg(conn, message)
+
+                if message.startswith("LEAVE_CHATROOM"):
+                    process_leave_msg(conn, message)
 
 
 def process_join_msg(conn, message):
@@ -89,7 +100,7 @@ def process_join_msg(conn, message):
     if msg_components is not None:
         create_chat_room(conn, msg_components.groups()[0], msg_components.groups()[3])
     else:
-        send_error_msg_to_client(5, error_code_5)
+        send_error_msg_to_client(error_code_1, 1, conn)
 
 
 def process_hello_msg(conn, message, addr):
@@ -108,7 +119,24 @@ def process_hello_msg(conn, message, addr):
         send_msg_to_client("HELO " + msg_components.groups()[0] + "\nIP:" + str(addr[0]) + "\nPort:" + str(
             addr[1]) + "\nStudentID:" + "17310876", conn)
     else:
-        send_error_msg_to_client(5, error_code_5)
+        send_error_msg_to_client(error_code_1, 1, conn)
+
+
+def process_leave_msg(conn, message):
+    """function to process leave chat room request from client
+
+        Args:
+            conn: the connection parameter acting as a link b/w client and server
+            message: message string from the client
+
+    """
+
+    # Matching the LEAVE message with the valid leave message template otherwise if false return error
+    msg_components = regex.match(valid_leave_msg, message, regex.M)
+    if msg_components is not None:
+        delete_from_chat_room(conn, msg_components.groups()[0], msg_components.groups()[1], msg_components.groups()[2])
+    else:
+        send_error_msg_to_client(error_code_1, 1, conn)
 
 
 def create_chat_room(conn, chat_room_name, chat_user_name):
@@ -130,15 +158,50 @@ def create_chat_room(conn, chat_room_name, chat_user_name):
     try:
         # Checking if chat room with hash id string exists if not create new
         if chat_room_id not in chat_rooms_array:
-            chat_rooms_array[chat_room_id] = ChatRoom.Chatroom(chat_room_name, chat_room_id, chat_user_name,
-                                                               chat_user_id)
+            chat_rooms_array[chat_room_id] = ChatRoom.Chatroom(chat_room_name, chat_room_id)
         room = chat_rooms_array[chat_room_id]
     finally:
         # Release the lock so that other thread can interact
         rooms_lock.release()
 
     print "RECEIVED FROM CLIENT-> Join request to join chat room named", chat_room_name, "from the user with username", chat_user_name
-    send_msg_to_client(room.add_user(conn, port, host), conn)
+    #  Send chat room details and response to the client
+    send_msg_to_client(room.add_user_to_chat_room(conn, port, host, chat_user_id, chat_user_name), conn)
+    #  Send message to all the users of the chat room- a new user has joined
+    room.send_chat_msg(chat_user_name, chat_user_name + "has joined the chat room")
+
+
+def delete_from_chat_room(conn, chat_room_id, chat_user_id, chat_user_name):
+    """function to delete a user assocaited from a specific chat room
+
+    Args:
+        conn: the connection parameter acting as a link b/w client and server
+        chat_room_id: the id uniquely associated to particular chat room
+        chat_user_id: id of the user who has to be delete from chat room
+        chat_user_name: unique name associated with each user
+
+    """
+
+    room = None
+
+    # Lock this portion such that no other thread can interfere
+    rooms_lock.acquire()
+    try:
+        # Checking if chat room with hash id string exists if not throw an error
+        if chat_room_id not in chat_rooms_array:
+            send_error_msg_to_client(error_code_1 + chat_room_id, 1, conn)
+        #  If exist fetch the details in the object
+        room = chat_rooms_array[chat_room_id]
+    finally:
+        # Release the lock so that other thread can interact
+        rooms_lock.release()
+
+    # Send response back to the client that leave process was successful
+    send_msg_to_client("LEFT_CHATROOM: " + str(chat_room_id) + "\nJOIN_ID: " + str(chat_user_id), conn)
+    #  Inform other users in the chat room that a particular user has left
+    room.send_chat_msg(chat_user_name, chat_user_name + " has left this chatroom.")
+    #  Remove user details from chat room
+    room.remove_user_from_chat_room(chat_user_id, chat_user_name, conn)
 
 
 def send_error_msg_to_client(err_desc, err_code, conn):
@@ -153,7 +216,7 @@ def send_error_msg_to_client(err_desc, err_code, conn):
 
     print "SENDING ERROR-> Error Code:", str(err_code), "with description", err_desc
     message = "ERROR_CODE: " + str(err_code) + "\nERROR_DESCRIPTION: " + err_desc
-    send_error_msg_to_client(message, conn)
+    send_msg_to_client(message, conn)
 
 
 def send_msg_to_client(message, conn):
@@ -165,7 +228,7 @@ def send_msg_to_client(message, conn):
 
     """
 
-    print "SENDING MESSAGE TO CLIENT->", message
+    print "SENDING MESSAGE TO CLIENT->\n", message
     if conn:
         conn.sendall((message + "\n").encode())
 
@@ -173,11 +236,11 @@ def send_msg_to_client(message, conn):
 # Main Method: First method to be called
 def server_main():
     global port
-    port = 8080  # Port for connection
+    port = 8181  # Port for connection
     # Ipv4 Socket Family and TCP Socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))  # Bind with port and host
-    s.listen(2)
+    s.listen(5)
 
     # continuous loop to keep accepting client requests
     while True:
